b83497f55fa469e92dc881cee5efd5cd4e7bb20e
nelson boneks <nelsonbonekeh925@gmail.com>
2024-07-13 12:44:05 -0500

    s3 bucket name modified

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "infra/terraformstatefile"
-    bucket         = "nelson-terraform-backend-bucket"
+    bucket         = "nelson-terra-backend-bucket"
     region         = "us-east-2"
     dynamodb_table = "terraform-state-locking"
   }
